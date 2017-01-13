7552701fb12317f68de35e26c486c5b52e5c8fe6
Jeremy Marshall Abbott <jeremymabbott@gmail.com>
2017-01-12 19:23:49 -0800

    Cleans up a few questions and the videos

---
+++ src/Migrations/Scripts/14_QuestionCleanup.sql
@@ -0,0 +1,155 @@
+DECLARE @surveyId INT;
+
+SELECT @surveyId = s.Id
+FROM dbo.Surveys s
+WHERE s.Name = 'Deaf Follower';
+
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/MwumddMxOxk' WHERE Number = 1 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/b9GQ5fa7yC8' WHERE Number = 2 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/rceIG2RG8CQ' WHERE Number = 3 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/ombE0ssVTw8' WHERE Number = 4 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/9vKfnni8DG8' WHERE Number = 5 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/AV4OaISkLmE' WHERE Number = 6 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/-wZITWkK6fM' WHERE Number = 7 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/BtNnsswOhLI' WHERE Number = 8 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/Dz_5N6uq5eU' WHERE Number = 9 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/YQC-A1QOGtU' WHERE Number = 10 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/xs6GwSz65gA' WHERE Number = 11 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/magHaK47SCk' WHERE Number = 12 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/ouYuMdxdCno' WHERE Number = 13 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/LO321iltHQY' WHERE Number = 14 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/ykrKNnuugfU' WHERE Number = 15 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/bu01OM-jS6o' WHERE Number = 16 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/vJqxqC15rUY' WHERE Number = 17 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/DhZzBsyXcFo' WHERE Number = 18 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/6raiKyb-9LU' WHERE Number = 19 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/z3iBpFSuC0Q' WHERE Number = 20 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/hiZRytaxBMY' WHERE Number = 21 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/IW-Dlq5o5jA' WHERE Number = 22 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/uBzJ7Uvp6M4' WHERE Number = 23 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/qu2dLnALpIg' WHERE Number = 24 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/MJJeFY1qSBU' WHERE Number = 25 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/HcHX_kwa3o4' WHERE Number = 26 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/8c1pRPCY1-8' WHERE Number = 27 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/1vjxNBWIOQQ' WHERE Number = 28 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/WKASIkmJrTI' WHERE Number = 29 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/dh2htVnMcmw' WHERE Number = 30 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/GFnMm0it-Bc' WHERE Number = 31 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/Rtx3hlZOv-w' WHERE Number = 32 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/oL4bCqINdkI' WHERE Number = 33 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/VvIHeqF3x10' WHERE Number = 34 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/0HdzscbTg1Q' WHERE Number = 35 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/6Eex4taBUXQ' WHERE Number = 36 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/r01j7ewHmJc' WHERE Number = 37 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/3fbG4KikcR8' WHERE Number = 38 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/HcCXTetcDl4' WHERE Number = 39 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/2QHi9X1hVuA' WHERE Number = 40 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/-dAznBvX7o4' WHERE Number = 41 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/5jLWebyZozw' WHERE Number = 42 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/Jf4Pnhn2yPA' WHERE Number = 43 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/sAt-hfLjaR0' WHERE Number = 44 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/5O8IDqLDDU0' WHERE Number = 45 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/6d87bDkDUZI' WHERE Number = 46 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/xL5gzkLybaU' WHERE Number = 47 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/0YiB4rZR-Tw' WHERE Number = 48 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/PwVidt0zv2E' WHERE Number = 49 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/2jRVeex1koM' WHERE Number = 50 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/0qZB40bLSGw' WHERE Number = 51 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/-_j-7FL47ck' WHERE Number = 52 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/00KK74eWLhU' WHERE Number = 53 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/qhYdF704SdU' WHERE Number = 54 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/vuuJ_aal78s' WHERE Number = 55 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/MknM0Ix6plY' WHERE Number = 56 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/14pG5txkZuM' WHERE Number = 57 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/hHwkxpJm1i0' WHERE Number = 58 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/7maI2HhF1AM' WHERE Number = 59 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/o0Wzr6V7jg8' WHERE Number = 60 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/vbuQRJNSvBw' WHERE Number = 61 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/ZZ_HMRIgIpQ' WHERE Number = 62 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/UBVEdPbaduU' WHERE Number = 63 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/_sepkrciw-o' WHERE Number = 64 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/xpH2UGLqoHU' WHERE Number = 65 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/IgEjsi5YnSc' WHERE Number = 66 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/n2cGn-4Kc44' WHERE Number = 67 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/1aNe-CV7jEk' WHERE Number = 68 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/HqXpsHPWS9Q' WHERE Number = 69 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/gEKWnF-K-sg' WHERE Number = 70 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/2-vPFrrYvEs' WHERE Number = 71 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/e45pX4SIG18' WHERE Number = 72 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/fME2J6acVt4' WHERE Number = 73 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/nn1zMp4OoHU' WHERE Number = 74 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/8ncfWl4Tkrc' WHERE Number = 75 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/A6CBeRc5o9o' WHERE Number = 76 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/GdUflTSkduo' WHERE Number = 77 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/TpSX0R4Wf0U' WHERE Number = 78 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/EuCSLKlSM2M' WHERE Number = 79 AND SurveyId = @surveyId
+UPDATE dbo.Questions SET VideoUrl = 'https://www.youtube.com/embed/WYxClxY7hhc' WHERE Number = 80 AND SurveyId = @surveyId
+
+UPDATE Surveys
+SET Name = 'DEAF'
+WHERE Name = 'Deaf Follower';
+
+UPDATE Surveys
+SET Name = 'HEARING'
+WHERE Name = 'Hearing Leader';
+
+UPDATE q
+SET q.[Required] = 1
+FROM dbo.Questions q
+JOIN dbo.Surveys s
+	ON q.SurveyId = s.Id
+	AND s.Name = 'HEARING'
+	AND q.Number IN (17, 18);
+
+
+DECLARE @questionId INT,
+		@optionId INT;
+
+SELECT @questionId = q.Id
+FROM dbo.Questions q
+JOIN dbo.QuestionOptions qo
+	ON q.Id = qo.QuestionId
+	AND q.Number = '75'
+JOIN dbo.Surveys s
+	ON q.SurveyId = s.Id
+	AND s.Name = 'HEARING'
+JOIN dbo.Options o
+	ON qo.OptionId = o.Id;
+
+SELECT @optionId = o.Id
+FROM dbo.Options o
+WHERE o.Value = 'Both Deaf and Hard of Hearing';
+
+INSERT INTO dbo.QuestionOptions(QuestionId, OptionId, [Weight]) VALUES (@questionId, @optionId, 0);
+
+SELECT @questionId = q.Id
+FROM dbo.Questions q
+JOIN dbo.QuestionOptions qo
+	ON q.Id = qo.QuestionId
+	AND q.Number = '77'
+JOIN dbo.Surveys s
+	ON q.SurveyId = s.Id
+	AND s.Name = 'DEAF'
+JOIN dbo.Options o
+	ON qo.OptionId = o.Id;
+
+SELECT @optionId = o.Id
+FROM dbo.Options o
+WHERE o.Value = 'I am single';
+
+INSERT INTO dbo.QuestionOptions(QuestionId, OptionId, [Weight]) VALUES (@questionId, @optionId, 0);
+
+SELECT @questionId = q.Id
+FROM dbo.Questions q
+JOIN dbo.QuestionOptions qo
+	ON q.Id = qo.QuestionId
+	AND q.Number = '72'
+JOIN dbo.Surveys s
+	ON q.SurveyId = s.Id
+	AND s.Name = 'DEAF'
+JOIN dbo.Options o
+	ON qo.OptionId = o.Id;
+
+UPDATE dbo.Questions
+SET Question = 'Which best describes your parents?'
+WHERE Id = @questionId;
